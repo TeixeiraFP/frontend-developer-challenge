1a524939381a3a38df9a4f7f7eb843a58baba764
topre <fpteixeira@outlook.com.br>
2021-10-06 20:22:24 -0300

    passando a desenvolver em js puro

--- validate.js
+++ validate.js
@@ -1,31 +1,27 @@
-const inputName = $("#input-name");
-const inputEmail = $("#input-email");
-const inputCpf = $("#input-cpf");
+const isName = document.querySelector("#input-name");
+const isEmail = document.querySelector("#input-email");
+const isCpf = document.querySelector("#input-cpf");
 
-//validate first forms
-$(".btn-registration").on("click", (e) => {
-  e.preventDefault();
-  try {
-    isValidName(inputName.val());
-    isValidEmail(inputEmail.val());
-    isvalidCPF(inputCpf.val());
-  } catch (err) {
-    console.log(err);
-  }
-  console.log("evento ok");
-});
+//submission event registration form
+document
+  .querySelector(".btn-registration")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
+    isValidName(isName.value);
+    isValidEmail(isEmail.value);
+    console.log(isCpf.value);
+  });
 
-//validate second forms
-$(".btn-register-friende").on("click", (e) => {
-  e.preventDefault();
-  try {
-    isValidName(inputName.val());
-    isValidEmail(inputEmail.val());
-  } catch (err) {
-    console.log(err);
-  }
-  console.log("evento ok");
-});
+const isEmailFriend = document.querySelector("#input-friend-email");
+const isNameFriend = document.querySelector("#input-friend-name");
+//friend registration submission event
+document
+  .querySelector(".btn-register-friende")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
+    isValidName(isNameFriend.value);
+    isValidEmail(isEmailFriend.value);
+  });
 
 // validation functions
 function isValidName(name) {
@@ -40,16 +36,16 @@ function isValidEmail(email) {
   else throw new Error("Email inválido");
 }
 
-// function isvalidCPF(cpf) {
-//   const validate = cpf.split("");
-//   const number = parseInt(cpf);
+function isvalidCPF(cpf) {
+  const validate = cpf.split("");
+  const number = parseInt(cpf);
 
-//   if (number === "Number")
-//     throw new Error("CPF inválido! Deve conter apenas números");
-//   else if (validate.length <= 9 && validate.length > 10)
-//     throw new Error("CPF inválido!");
-//   //   else console.log(cpf);
+  if (number === "Number")
+    throw new Error("CPF inválido! Deve conter apenas números");
+  else if (validate.length <= 9 && validate.length > 10)
+    throw new Error("CPF inválido!");
+  //   else console.log(cpf);
 
-//   console.log(parseInt(cpf));
-//   console.log(validate);
-// }
+  console.log(parseInt(cpf));
+  console.log(validate);
+}
