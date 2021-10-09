292b3966dc267f97e74a3b744e0ddf7d03bc9751
topre <fpteixeira@outlook.com.br>
2021-10-09 20:18:08 -0300

    cards posicionados na tela, responsividade implementada

--- request.js
+++ request.js
@@ -11,9 +11,45 @@ function search() {
 search();
 
 function card(data) {
-  const result = data.map((element) => {
-    picture.src = element.image;
-    prodName.textContent = element.name;
-    prodDescr.textContent = element.description;
-  });
+  for (let value of data) {
+    const article = document.createElement("article");
+    article.setAttribute("class", "card");
+
+    const html = `<section class="container-img">
+      <img class="card-img" src="https:${value.image}" alt="img" />
+    </section>
+    <section class="container-content">
+      <h4 class="title-card-content">${value.name}</h4>
+      <p class="description-content invisible">${value.description}</p>
+      <p class="old-price">De: R$${value.oldPrice}</p>
+      <p class="price">Por: R$${value.price}</p>
+      <p class="division-price">Ou: ${value.installments.count} de R$:${value.installments.value}</p>
+      <button class="btn-card-content">Comprar</button>
+    </section>
+    `;
+    article.innerHTML = html;
+    document.querySelector(".container-card").appendChild(article);
+  }
 }
+
+// for (let product of data.products) {
+//   const article = document.createElement('article');
+//   article.setAttribute('class', 'card');
+//   const html =
+//   `<section class="card-image-container">
+//       <img class="card-image" src="https:${product.image}" alt="Image">
+//   </section>
+//   <section class="card-content-container">
+//       <h3>${product.name}</h3>
+//       <p>${product.description}</p>
+//       <p>De: R$${product.oldPrice}</p>
+//       <p class="price">Por: R$${product.price}</p>
+//       <p>ou ${product.installments.count}x de R$${product.installments.value}</p>
+//       <button>Comprar</button>
+//   </section>`;
+//   article.innerHTML = html;
+//   document.querySelector('.card-container').appendChild(article);
+// }
+// endpoint = 'https://' + data.nextPage;
+// });
+// }
